ea1421e1cef4c7464677e4d39946e88eed812ab5
Shadab Khan <shadabkhan7253@gmail.com>
2023-11-24 18:05:37 +0530

    Header Created

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,62 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import Logo from '../assets/ProjectTracker_logo.png';
 
 export const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header>
+      <nav className=" bg-red-500  dark:bg-gray-800 dark:border-gray-700">
+        <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+          <Link to="/" className="flex items-center space-x-3 rtl:space-x-reverse">
+            <img src={Logo} className="h-14 " alt="Project  Tracker Logo" />
+          </Link>
+          <button
+            data-collapse-toggle="navbar-solid-bg"
+            type="button"
+            className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+            aria-controls="navbar-solid-bg"
+            aria-expanded="false"
+          >
+            <span className="sr-only">Open main menu</span>
+            <svg
+              className="w-5 h-5"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 17 14"
+            >
+              <path
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M1 1h15M1 7h15M1 13h15"
+              />
+            </svg>
+          </button>
+          <div className="hidden w-full md:block md:w-auto" id="navbar-solid-bg">
+            <ul className="flex flex-col font-medium mt-4 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-transparent dark:bg-gray-800 md:dark:bg-transparent dark:border-gray-700">
+              <li>
+                <Link
+                  to="/"
+                  className="block py-2 px-3 md:p-0 text-white text-xl hover:text-slate-800"
+                  aria-current="page"
+                >
+                  Home
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="/projectDetails"
+                  className="block py-5 px-3 md:p-0 text-white text-xl hover:text-slate-800  "
+                >
+                  Project Details
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
 };
