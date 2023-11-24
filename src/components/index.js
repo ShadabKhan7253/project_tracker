124f6a768f5b5259c3bf0c1edcc0cc235e19a736
Shadab Khan <shadabkhan7253@gmail.com>
2023-11-24 23:41:36 +0530

    export the component

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,5 @@
 export { Header } from './Header';
 export { ProjectCard } from './ProjectCard';
+export { Sidebar } from './Sidebar';
+export { EditableProjectForm } from './EditableProjectForm';
 export { Footer } from './Footer';
