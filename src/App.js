0099b3ea6f5f7c5fe5fcb0c8e2b245e60e4b72d6
Shadab Khan <shadabkhan7253@gmail.com>
2023-11-25 15:25:39 +0530

    method created to handle the project creation

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
 import { useState } from 'react';
 import { Footer, Header } from './components';
 import { AppRoutes } from './routes/AppRoutes';
+import { v1 as uuidv4 } from 'uuid';
 
 function App() {
   const [isSidebarVisible, setIsSidebarVisible] = useState(false);
+  const [projects, setProjects] = useState([]);
 
   const handleSideBar = () => {
     setIsSidebarVisible(!isSidebarVisible);
   };
 
+  const handleAddProject = (project) => {
+    const projectToBeAdded = { ...project, id: uuidv4(), elapsed: 0, runningSince: null };
+    setProjects([...projects, projectToBeAdded]);
+  };
+
   return (
     <>
-      <Header onSidebarVisible={handleSideBar} isSidebarVisible={isSidebarVisible} />
-      <AppRoutes />
+      <Header
+        onSidebarVisible={handleSideBar}
+        isSidebarVisible={isSidebarVisible}
+        onAddProject={handleAddProject}
+      />
+      <AppRoutes projects={projects} />
       <Footer />
     </>
   );
