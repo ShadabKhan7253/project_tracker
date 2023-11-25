2e00edeb196e16c1f783b0985008e182e1675bef
Shadab Khan <shadabkhan7253@gmail.com>
2023-11-25 12:27:52 +0530

    initial dummy task created and render the ProjectList component

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,5 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { ProjectList } from '../components';
+
+const initialProjects = [
+  {
+    id: 1,
+    name: 'Project 1',
+    description:
+      'Project 1 Description Project 1 Description Project 1 Description Project 1 Description',
+    status: 'Ongoing',
+    price: 45,
+    elapsed: 46446737,
+    runningSince: null,
+  },
+  {
+    id: 2,
+    name: 'Project 2',
+    description: 'Project 2 Description Project 2 Description ',
+    status: 'Ongoing',
+    price: 55,
+    elapsed: 46446737,
+    runningSince: 1700488695674,
+  },
+];
 
 export const HomePage = () => {
-  return <div>HomePage</div>;
+  const [projects, setProjects] = useState(initialProjects);
+  return (
+    <>
+      <ProjectList projects={projects} />
+    </>
+  );
 };
