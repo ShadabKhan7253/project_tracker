f32d17cf75675ca7d3922d273ba5eae116ef7fb8
Shadab Khan <shadabkhan7253@gmail.com>
2023-11-25 12:28:32 +0530

    ProjectList component created with hold the list of projects

---
+++ src/components/ProjectList.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { ProjectCard } from './ProjectCard';
+
+export const ProjectList = ({ projects }) => {
+  console.log(projects);
+  return (
+    <main>
+      <div className="flex flex-wrap ml-10 gap-x-10 gap-y-10">
+        {projects.map((project) => (
+          <ProjectCard key={project.id} project={project} />
+        ))}
+      </div>
+    </main>
+  );
+};
