3ff66cb18abbf2e41c9898589f78c9ca22747d3d
Shadab Khan <shadabkhan7253@gmail.com>
2023-11-25 12:26:59 +0530

    PorjectList Component exported

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,6 @@
 export { Header } from './Header';
 export { ProjectCard } from './ProjectCard';
+export { ProjectList } from './ProjectList';
 export { Sidebar } from './Sidebar';
 export { EditableProjectForm } from './EditableProjectForm';
 export { Footer } from './Footer';
